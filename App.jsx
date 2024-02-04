1e1fa83581c1c7f330b9914994c3c5eed6973d7c
yuvrajverma8 <125306873+yuvrajverma8@users.noreply.github.com>
2024-02-04 14:07:15 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,54 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import ToDoForm from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
+
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default App;
